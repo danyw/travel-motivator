f90f70b56bf2bb2dc8427b4e770bf7e0c99130d6
danyw <danielwiejak@gmail.com>
2023-02-05 18:05:03 +0000

    snd sortable works, but beautiful-dnd is not supported by react 18 and <React.StrictMode> tag needed to be commented out

--- src/components/DestinationList.js
+++ src/components/DestinationList.js
@@ -1,12 +1,40 @@
 import React from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const DestinationList = ({ destinations }) => {
   return (
-    <ul>
-      {destinations.map((destination, index) => (
-        <li key={index}>{destination}</li>
-      ))}
-    </ul>
+    <DragDropContext>
+      <Droppable droppableId="destinations">
+        {(provided, snapshot) => (
+          
+          <ul 
+          className="destinations" 
+        //   {...provided.droppableProps} 
+          ref={provided.innerRef}>
+
+            {destinations.map((destination, index) => (
+                <Draggable 
+                key={destination} 
+                draggableId={destination} 
+                index={index}>
+
+                    {(provided, snapshot) => (
+              <li 
+              key={index} 
+              {...provided.dragHandleProps} 
+              {...provided.draggableProps} 
+              ref={provided.innerRef}>
+                
+                {destination}
+                </li>
+              )}
+              </Draggable>
+            ))}
+          </ul>
+         
+        )} 
+      </Droppable>
+    </DragDropContext>
   );
 };
 
