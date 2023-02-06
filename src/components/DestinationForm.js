05e9f2dadda126a1b3d332b6330586f701a3ff8d
danyw <danielwiejak@gmail.com>
2023-02-06 23:45:35 +0000

    Destination list work, add error when duplicate

--- src/components/DestinationForm.js
+++ src/components/DestinationForm.js
@@ -1,19 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import { useLoadScript, Autocomplete } from "@react-google-maps/api";
+
+const libraries = ["places"];
 
 const DestinationForm = ({ addDestination }) => {
   const [destination, setDestination] = useState("");
+  const [destinationList, setDestinationList] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const autocompleteRef = useRef();
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_Maps_API_Key,
+    libraries,
+  });
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    addDestination(destination);
+    if (e) {
+      e.preventDefault();
+    }
+
+    let selectedPlace;
+    if (autocompleteRef.current) {
+      selectedPlace = autocompleteRef.current.getPlace();
+    }
+
+    let newDestination = selectedPlace ? selectedPlace.name : destination;
+
+    if (!destinationList.includes(newDestination)) {
+      setDestinationList([...destinationList, newDestination]);
+      addDestination(newDestination);
+      setErrorMessage("");
+    } else {
+      setErrorMessage("This destination is already in the list.");
+    }
+
     setDestination("");
+  };
+
+  const onPlaceChanged = () => {
+    let selectedPlace;
+    if (autocompleteRef.current) {
+      selectedPlace = autocompleteRef.current.getPlace();
+    }
+    if (selectedPlace) {
+      setDestination(selectedPlace.name);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label>Destination:</label>
-      <input type="text" value={destination} onChange={(e) => setDestination(e.target.value)} placeholder="Type a destination" />
+      <Autocomplete
+        onLoad={(autocomplete) => {
+          autocompleteRef.current = autocomplete;
+        }}
+        onPlaceChanged={onPlaceChanged}
+      >
+        <input type="text" value={destination} onChange={(e) => setDestination(e.target.value)} placeholder="Type a destination" />
+      </Autocomplete>
+
       <button type="submit">Add</button>
+      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
     </form>
   );
 };
